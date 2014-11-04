ec003fa995f2b6000f724cca4c276f8c50bf5c3c
Salem Duy <SalemDuy@DuySalem-PC.appota>
2014-11-04 15:22:34 +0700

    update sample

--- sample/src/com/sample/onclansdk/SampleActivity.java
+++ sample/src/com/sample/onclansdk/SampleActivity.java
@@ -3,18 +3,33 @@ package com.sample.onclansdk;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.onclan.android.core.OnClanSDK;
+import com.onclan.android.core.OnClanSubType;
 
 public class SampleActivity extends Activity {
 	
-	OnClanSDK onClanSDK;
+	private OnClanSDK onClanSDK;
 	
 	@Override
 	protected void onCreate(Bundle arg0) {
 		super.onCreate(arg0);
 		setContentView(R.layout.activity_sample);
-		onClanSDK = OnClanSDK.getInstance().initialize(this);
+		String[] onClanFeatures = new String[] {OnClanSubType.TYPE_CHAT, OnClanSubType.TYPE_LEADERBOARD};
+		onClanSDK = OnClanSDK.getInstance().initialize(this, onClanFeatures);
+	}
+	
+	public void showHome(View v){
+		onClanSDK.showHome();
+	}
+	
+	public void showChat(View v){
+		onClanSDK.showChat();
+	}
+	
+	public void showLeaderboard(View v){
+		onClanSDK.showLeaderBoard();
 	}
 
 	@Override
